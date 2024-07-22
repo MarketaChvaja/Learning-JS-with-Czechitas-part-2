0c23c5e80228bb40a1f3e24ec5315cfdd42bdf3d
Marketa Chvaja <meridian.czech@gmail.com>
2024-07-22 19:10:00 +0200

    files by czechitas added

---
+++ 02-dom/dom.js
@@ -0,0 +1,17 @@
+let html = `
+	<p class="prvni">
+		Bonbon cake jelly-o croissant tiramisu dessert.
+		Macaroon cookie ice cream sweet roll topping fruitcake candy canes macaroon sweet roll.
+		Croissant cookie jelly-o donut cupcake sugar plum.
+		Chocolate brownie chocolate cake icing tart marzipan tootsie roll chocolate cheesecake.
+	</p>
+	<p class="druhy">
+		Carrot cake pastry chupa chups cheesecake. Jujubes candy canes cake lemon drops.
+		Powder gingerbread gummi bears gingerbread gingerbread. Bonbon muffin danish.
+	</p>
+	<p class="treti">
+		Marshmallow chocolate bar ice cream cookie wafer gingerbread cookie pastry.
+		Candy biscuit soufflé marshmallow pastry donut. Lollipop gummies danish sesame snaps.
+		Croissant tart candy canes gingerbread powder bear claw.
+	</p>
+`;
